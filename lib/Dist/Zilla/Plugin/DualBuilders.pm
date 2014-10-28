6351ec4c1055db35a45e456d00f8022bc47f4cbb
Apocalypse <perl@0ne.us>
2014-10-28 18:21:26 +0000

    Import of APOCAL/Dist-Zilla-Plugin-DualBuilders-1.002 from CPAN.
    gitpan-cpan-distribution: Dist-Zilla-Plugin-DualBuilders
    gitpan-cpan-version:      1.002
    gitpan-cpan-path:         APOCAL/Dist-Zilla-Plugin-DualBuilders-1.002.tar.gz
    gitpan-cpan-author:       APOCAL
    gitpan-cpan-maturity:     released

--- lib/Dist/Zilla/Plugin/DualBuilders.pm
+++ lib/Dist/Zilla/Plugin/DualBuilders.pm
@@ -1,21 +1,18 @@
 #
 # This file is part of Dist-Zilla-Plugin-DualBuilders
 #
-# This software is copyright (c) 2010 by Apocalypse.
+# This software is copyright (c) 2014 by Apocalypse.
 #
 # This is free software; you can redistribute it and/or modify it under
 # the same terms as the Perl 5 programming language system itself.
 #
 use strict; use warnings;
 package Dist::Zilla::Plugin::DualBuilders;
-BEGIN {
-  $Dist::Zilla::Plugin::DualBuilders::VERSION = '1.001';
-}
-BEGIN {
-  $Dist::Zilla::Plugin::DualBuilders::AUTHORITY = 'cpan:APOCAL';
-}
+# git description: release-1.001-5-g9a83558
+$Dist::Zilla::Plugin::DualBuilders::VERSION = '1.002';
+our $AUTHORITY = 'cpan:APOCAL';
 
-# ABSTRACT: Allows use of Module::Build and ExtUtils::MakeMaker in a dzil dist
+# ABSTRACT: (DEPRECATED) Allows use of Module::Build and ExtUtils::MakeMaker in a dzil dist
 
 use Moose 1.03;
 
@@ -23,6 +20,13 @@ with 'Dist::Zilla::Role::PrereqSource' => { -version => '3.101461' };
 with 'Dist::Zilla::Role::InstallTool' => { -version => '3.101461' };
 with 'Dist::Zilla::Role::AfterBuild' => { -version => '3.101461' };
 
+#pod =attr prefer
+#pod
+#pod Sets your preferred builder. This builder will be the one added to the prereqs. Valid options are: "make" or "build".
+#pod
+#pod The default is: build
+#pod
+#pod =cut
 
 {
 	use Moose::Util::TypeConstraints 1.01;
@@ -36,6 +40,14 @@ with 'Dist::Zilla::Role::AfterBuild' => { -version => '3.101461' };
 	no Moose::Util::TypeConstraints;
 }
 
+#pod =attr block_test
+#pod
+#pod This is a boolean value determining if we will block both testers from running the testsuite. If you have both
+#pod builders loaded, you will run the testsuite twice! If you want this behavior, please set this value to false.
+#pod
+#pod The default is: true
+#pod
+#pod =cut
 
 has block_test => (
 	is => 'ro',
@@ -54,7 +66,7 @@ has _makever => (
 );
 
 sub setup_installer {
-	my ($self, $file) = @_;
+	my $self = shift;
 
 	# This is to munge the files
 	foreach my $file ( @{ $self->zilla->files } ) {
@@ -120,7 +132,7 @@ sub register_prereqs {
 }
 
 sub after_build {
-        my( $self, $root ) = @_;
+        my $self = shift;
 
         return if ! $self->block_test;
 
@@ -154,23 +166,35 @@ no Moose;
 __PACKAGE__->meta->make_immutable;
 1;
 
-
 __END__
+
 =pod
+
+=encoding UTF-8
+
+=for :stopwords Apocalypse cpan testmatrix url annocpan anno bugtracker rt cpants kwalitee
+diff irc mailto metadata placeholders metacpan MakeMaker ModuleBuild dist
+dzil prereq prereqs
 
 =for Pod::Coverage register_prereqs setup_installer after_build
 
-=for stopwords MakeMaker ModuleBuild dist dzil prereq prereqs
-
 =head1 NAME
 
-Dist::Zilla::Plugin::DualBuilders - Allows use of Module::Build and ExtUtils::MakeMaker in a dzil dist
+Dist::Zilla::Plugin::DualBuilders - (DEPRECATED) Allows use of Module::Build and ExtUtils::MakeMaker in a dzil dist
 
 =head1 VERSION
 
-  This document describes v1.001 of Dist::Zilla::Plugin::DualBuilders - released December 15, 2010 as part of Dist-Zilla-Plugin-DualBuilders.
+  This document describes v1.002 of Dist::Zilla::Plugin::DualBuilders - released October 28, 2014 as part of Dist-Zilla-Plugin-DualBuilders.
 
 =head1 DESCRIPTION
+
+***DEPRECATED***
+
+While this plugin still works, it did not work cleanly for everyone and it's preferred for you to pick one builder and stick with it! The latest
+toolchain updates have ensured that modern Perls are able to handle either builder. Therefore there is no longer the need to include both
+F<Build.PL> and F<Makefile.PL> in the dist. If you want to provide support for older Perls, please use L<Dist::Zilla::Plugin::MakeMaker::Fallback>.
+
+=head2 OLD POD
 
 This plugin allows you to specify ModuleBuild and MakeMaker in your L<Dist::Zilla> F<dist.ini> and select
 only one as your prereq. Normally, if this plugin is not loaded you will end up with both in your prereq list
@@ -204,13 +228,17 @@ Please see those modules/websites for more information related to this module.
 
 =item *
 
-L<Dist::Zilla>
+L<Dist::Zilla|Dist::Zilla>
+
+=item *
+
+L<Dist::Zilla::Plugin::MakeMaker::Fallback|Dist::Zilla::Plugin::MakeMaker::Fallback>
 
 =back
 
-=for :stopwords CPAN AnnoCPAN RT CPANTS Kwalitee diff IRC
-
 =head1 SUPPORT
+
+=head2 Perldoc
 
 You can find documentation for this module with the perldoc command.
 
@@ -225,7 +253,17 @@ in addition to those websites please use your favorite search engine to discover
 
 =item *
 
+MetaCPAN
+
+A modern, open-source CPAN search engine, useful to view POD in HTML format.
+
+L<http://metacpan.org/release/Dist-Zilla-Plugin-DualBuilders>
+
+=item *
+
 Search CPAN
+
+The default CPAN search engine, useful to view POD in HTML format.
 
 L<http://search.cpan.org/dist/Dist-Zilla-Plugin-DualBuilders>
 
@@ -233,11 +271,15 @@ L<http://search.cpan.org/dist/Dist-Zilla-Plugin-DualBuilders>
 
 RT: CPAN's Bug Tracker
 
+The RT ( Request Tracker ) website is the default bug/issue tracking system for CPAN.
+
 L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Dist-Zilla-Plugin-DualBuilders>
 
 =item *
 
-AnnoCPAN: Annotated CPAN documentation
+AnnoCPAN
+
+The AnnoCPAN is a website that allows community annotations of Perl module documentation.
 
 L<http://annocpan.org/dist/Dist-Zilla-Plugin-DualBuilders>
 
@@ -245,33 +287,55 @@ L<http://annocpan.org/dist/Dist-Zilla-Plugin-DualBuilders>
 
 CPAN Ratings
 
+The CPAN Ratings is a website that allows community ratings and reviews of Perl modules.
+
 L<http://cpanratings.perl.org/d/Dist-Zilla-Plugin-DualBuilders>
 
 =item *
 
 CPAN Forum
 
+The CPAN Forum is a web forum for discussing Perl modules.
+
 L<http://cpanforum.com/dist/Dist-Zilla-Plugin-DualBuilders>
 
 =item *
 
-CPANTS Kwalitee
+CPANTS
 
-L<http://cpants.perl.org/dist/overview/Dist-Zilla-Plugin-DualBuilders>
+The CPANTS is a website that analyzes the Kwalitee ( code metrics ) of a distribution.
+
+L<http://cpants.cpanauthors.org/dist/overview/Dist-Zilla-Plugin-DualBuilders>
 
 =item *
 
-CPAN Testers Results
+CPAN Testers
 
-L<http://cpantesters.org/distro/D/Dist-Zilla-Plugin-DualBuilders.html>
+The CPAN Testers is a network of smokers who run automated tests on uploaded CPAN distributions.
+
+L<http://www.cpantesters.org/distro/D/Dist-Zilla-Plugin-DualBuilders>
 
 =item *
 
 CPAN Testers Matrix
 
+The CPAN Testers Matrix is a website that provides a visual overview of the test results for a distribution on various Perls/platforms.
+
 L<http://matrix.cpantesters.org/?dist=Dist-Zilla-Plugin-DualBuilders>
 
+=item *
+
+CPAN Testers Dependencies
+
+The CPAN Testers Dependencies is a website that shows a chart of the test results of all dependencies for a distribution.
+
+L<http://deps.cpantesters.org/?module=Dist::Zilla::Plugin::DualBuilders>
+
 =back
+
+=head2 Email
+
+You can email the author of this module at C<APOCAL at cpan.org> asking for help with any problems you have.
 
 =head2 Internet Relay Chat
 
@@ -305,8 +369,8 @@ You can connect to the server at 'irc.efnet.org' and join this channel: #perl th
 =head2 Bugs / Feature Requests
 
 Please report any bugs or feature requests by email to C<bug-dist-zilla-plugin-dualbuilders at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Dist-Zilla-Plugin-DualBuilders>.  I will be
-notified, and then you'll automatically be notified of progress on your bug as I make changes.
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Dist-Zilla-Plugin-DualBuilders>. You will be automatically notified of any
+progress on the request by the system.
 
 =head2 Source Code
 
@@ -314,7 +378,7 @@ The code is open to the world, and available for you to hack on. Please feel fre
 with it, or whatever. If you want to contribute patches, please send me a diff or prod me to pull
 from your repository :)
 
-L<http://github.com/apocalypse/perl-dist-zilla-plugin-dualbuilders>
+L<https://github.com/apocalypse/perl-dist-zilla-plugin-dualbuilders>
 
   git clone git://github.com/apocalypse/perl-dist-zilla-plugin-dualbuilders.git
 
@@ -324,12 +388,33 @@ Apocalypse <APOCAL@cpan.org>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2010 by Apocalypse.
+This software is copyright (c) 2014 by Apocalypse.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
 
-The full text of the license can be found in the LICENSE file included with this distribution.
+The full text of the license can be found in the
+F<LICENSE> file included with this distribution.
+
+=head1 DISCLAIMER OF WARRANTY
+
+THERE IS NO WARRANTY FOR THE PROGRAM, TO THE EXTENT PERMITTED BY
+APPLICABLE LAW.  EXCEPT WHEN OTHERWISE STATED IN WRITING THE COPYRIGHT
+HOLDERS AND/OR OTHER PARTIES PROVIDE THE PROGRAM "AS IS" WITHOUT WARRANTY
+OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+PURPOSE.  THE ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE PROGRAM
+IS WITH YOU.  SHOULD THE PROGRAM PROVE DEFECTIVE, YOU ASSUME THE COST OF
+ALL NECESSARY SERVICING, REPAIR OR CORRECTION.
+
+IN NO EVENT UNLESS REQUIRED BY APPLICABLE LAW OR AGREED TO IN WRITING
+WILL ANY COPYRIGHT HOLDER, OR ANY OTHER PARTY WHO MODIFIES AND/OR CONVEYS
+THE PROGRAM AS PERMITTED ABOVE, BE LIABLE TO YOU FOR DAMAGES, INCLUDING ANY
+GENERAL, SPECIAL, INCIDENTAL OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE
+USE OR INABILITY TO USE THE PROGRAM (INCLUDING BUT NOT LIMITED TO LOSS OF
+DATA OR DATA BEING RENDERED INACCURATE OR LOSSES SUSTAINED BY YOU OR THIRD
+PARTIES OR A FAILURE OF THE PROGRAM TO OPERATE WITH ANY OTHER PROGRAMS),
+EVEN IF SUCH HOLDER OR OTHER PARTY HAS BEEN ADVISED OF THE POSSIBILITY OF
+SUCH DAMAGES.
 
 =cut
-
--- t/apocalypse.t
+++ t/apocalypse.t
@@ -2,7 +2,7 @@
 #
 # This file is part of Dist-Zilla-Plugin-DualBuilders
 #
-# This software is copyright (c) 2010 by Apocalypse.
+# This software is copyright (c) 2014 by Apocalypse.
 #
 # This is free software; you can redistribute it and/or modify it under
 # the same terms as the Perl 5 programming language system itself.
@@ -11,13 +11,10 @@ use strict; use warnings;
 use strict; use warnings;
 
 use Test::More;
-eval "use Test::Apocalypse 0.10";
+eval "use Test::Apocalypse 1.000";
 if ( $@ ) {
 	plan skip_all => 'Test::Apocalypse required for validating the distribution';
 } else {
-	# hack for Kwalitee ( zany require format so DZP::AutoPrereq will not pick it up )
-	require 'Test/NoWarnings.pm'; require 'Test/Pod.pm'; require 'Test/Pod/Coverage.pm';
-
 	is_apocalypse_here( {
 		
 	} );
